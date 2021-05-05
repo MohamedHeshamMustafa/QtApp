a4a8f8e771b5ea6a2b32f0e2b56df68a2d553acb
Mohamed Hesham <m.hesham@mcsoil.com>
2021-05-05 13:26:13 +0200

    Dark theme added

--- app/main.cpp
+++ app/main.cpp
@@ -2,13 +2,51 @@
 #include <iostream>
 // Qt
 #include <QApplication>
+#include <QStyleFactory>
 //Internal
 #include "MainWindow.h"
+
+void SetTheme(QApplication& App) {
+    App.setStyle(QStyleFactory::create("Fusion"));
+    // increase font size for better reading
+    QFont defaultFont = QApplication::font();
+    defaultFont.setPointSize(defaultFont.pointSize() + 2);
+    App.setFont(defaultFont);
+    // modify palette to dark
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Disabled, QPalette::WindowText, QColor(127, 127, 127));
+    darkPalette.setColor(QPalette::Base, QColor(42, 42, 42));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Disabled, QPalette::Text, QColor(127, 127, 127));
+    darkPalette.setColor(QPalette::Dark, QColor(35, 35, 35));
+    darkPalette.setColor(QPalette::Shadow, QColor(20, 20, 20));
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor(127, 127, 127));
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Disabled, QPalette::Highlight, QColor(80, 80, 80));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::white);
+    darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, QColor(127, 127, 127));
+
+    App.setPalette(darkPalette);
+
+}
+
 
 int main(int argc, char* argv[])
 {
     QApplication QApplication(argc, argv);
+    SetTheme(QApplication);
+    Q_INIT_RESOURCE(resources);
     MainWindow mainWindow;
     mainWindow.showMaximized();
+    Q_CLEANUP_RESOURCE(resources);
     return QApplication.exec();
 }
